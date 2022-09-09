c27c8b00d7e0956e5a38efd6c1be95b44671858b
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2022-09-09 20:51:18 +0300

    Refactor

--- energy_saver.lua
+++ energy_saver.lua
@@ -1,9 +1,8 @@
-local ____IL2CPP = require("MonsterJournalPage.IL2CPP.IL2CPP")
+local ____IL2CPP = require("energy_saver.IL2CPP.IL2CPP")
 local snow = ____IL2CPP.snow
 local via = ____IL2CPP.via
 local modMenuModule = "ModOptionsMenu.ModMenuApi"
 local config_path = "energy_saver.json"
-
 local fps_option_list = { 30, 60, 90, 120, 144, 165, 240, 600 }
 local config = { townFps = 60, unfocusedFps = 30 }
 local game_status = 1
@@ -13,17 +12,20 @@ function get_fps_option()
     return fps_option_list[index+1]
 end
 
+function set_max_fps()
+    if game_status == 1 then
+        via.Application:set_MaxFps(config.townFps+.0)
+    else
+        via.Application:set_MaxFps(selected_fps_option+.0)
+        -- find value for unlimited fps
+    end
+end
+
 function game_status_changed(game_status_new)
-    log.debug("Game status: " .. tostring(game_status_new))
     game_status = game_status_new
-    
     local selected_fps_option = get_fps_option()
     if selected_fps_option > config.townFps then
-        if game_status == 1 then
-            via.Application:set_MaxFps(config.townFps+.0)
-        else
-            via.Application:set_MaxFps(selected_fps_option+.0)
-        end
+        set_max_fps()
     end
 end
 
@@ -53,20 +55,15 @@ end
 
 sdk.hook(snow.QuestManager.onChangedGameStatus,
     function(args) game_status_changed(sdk.to_int64(args[3])); end,
-    function(retval) return retval; end);
+    function(retval) return retval; end)
 
 if is_module_available(modMenuModule) then
     modUI = require(modMenuModule);
 
     local changed = false
-    local name = "Energy Saver";
-    local description = "Options to limit the power usage of the game.";
-    local modObj = modUI.OnMenu(name, description, function()
+    modUI.OnMenu("Energy Saver", "Options to limit the power usage of the game.", function()
         changed, config.townFps = modUI.Slider("Town Framerate", config.townFps, 10, get_fps_option(), "Set framerate for town.")
-
-        if changed and game_status == 1 then
-            via.Application:set_MaxFps(config.townFps+.0)
-        end
+        if changed then set_max_fps() end
     end)
 end
 
