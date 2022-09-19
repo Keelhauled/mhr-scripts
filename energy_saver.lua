1947e8c16fa70648f5056440f5e9deb5543adf07
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2022-09-19 15:20:47 +0300

    Include Stracker's IL2CPP package

--- energy_saver.lua
+++ energy_saver.lua
@@ -1,17 +1,79 @@
-local ____IL2CPP = require("energy_saver.IL2CPP.IL2CPP")
-local snow = ____IL2CPP.snow
-local via = ____IL2CPP.via
+local metatable = (function()
+    local create_namespace
+    local namespace_functions = {}
 
-local max_fps = via.Application:get_MaxFps()
+    ---@param self Namespace
+    function namespace_functions.T(self)
+        return function(ns) return ns._typedef end
+    end
+
+    ---@param self Namespace
+    function namespace_functions.Instance(self)
+        return sdk.get_managed_singleton(self._name)
+    end
+
+    local namespace_builder_metatable = {
+        ---@param name string
+        __index = function(self, name)
+            -- Fallback for fields that can't be taken as symbols
+            if namespace_functions[name] then
+                return namespace_functions[name](self)
+            end
+            local typedef = rawget(self, "_typedef")
+            if typedef then
+                local field = typedef:get_field(name)
+                if field then
+                    if field:is_static() then
+                        return field:get_data()
+                    end
+                    return field
+                end
+
+                local method = typedef:get_method(name)
+                if method then
+                    return method
+                end
+            end
+            local force = false
+            if name:sub(1, 2) == "__" then
+                name  = name:sub(3)
+                force = true
+            end
+            return create_namespace(rawget(self, "_name") .. "." .. name, force)
+        end
+    }
+
+    create_namespace = function(basename, force_namespace)
+        force_namespace = force_namespace or false
+
+        ---@class Namespace
+        local table = { _name = basename }
+        if sdk.find_type_definition(basename) and not force_namespace then
+            table = { _typedef = sdk.find_type_definition(basename), _name = basename }
+        else
+            table = { _name = basename }
+        end
+        return setmetatable(table, namespace_builder_metatable)
+    end
+
+    return setmetatable({}, { __index = function(self, name)
+        return create_namespace(name)
+    end })
+end)()
+
+local snow = metatable.snow
+local app = metatable.via.Application
+
+local max_fps = app:get_MaxFps()
 
 local function limit_max_fps(args)
     if snow.QuestManager.Instance._QuestStatus == 0 then
-        via.Application:set_MaxFps(40.0)
+        app:set_MaxFps(40.0)
     end
 end
 
 local function reset_max_fps(args)
-    via.Application:set_MaxFps(max_fps)
+    app:set_MaxFps(max_fps)
 end
 
 local function empty_post_func(retval)
