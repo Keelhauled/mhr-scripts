156451d568d0e9de4e6c055d8c0b7795990cc294
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2022-11-17 01:51:07 +0200

    Add chara_edit_fix

---
+++ chara_edit_fix.lua
@@ -0,0 +1,82 @@
+local metatable = (function()
+    -- Metatable magic by Stracker
+
+    local create_namespace
+    local namespace_functions = {}
+
+    ---@param self Namespace
+    function namespace_functions.T(self)
+        return function(ns) return ns._typedef end
+    end
+
+    ---@param self Namespace
+    function namespace_functions.Instance(self)
+        return sdk.get_managed_singleton(self._name)
+    end
+
+    local namespace_builder_metatable = {
+        ---@param name string
+        __index = function(self, name)
+            -- Fallback for fields that can't be taken as symbols
+            if namespace_functions[name] then
+                return namespace_functions[name](self)
+            end
+            local typedef = rawget(self, "_typedef")
+            if typedef then
+                local field = typedef:get_field(name)
+                if field then
+                    if field:is_static() then
+                        return field:get_data()
+                    end
+                    return field
+                end
+
+                local method = typedef:get_method(name)
+                if method then
+                    return method
+                end
+            end
+            local force = false
+            if name:sub(1, 2) == "__" then
+                name  = name:sub(3)
+                force = true
+            end
+            return create_namespace(rawget(self, "_name") .. "." .. name, force)
+        end
+    }
+
+    create_namespace = function(basename, force_namespace)
+        force_namespace = force_namespace or false
+
+        ---@class Namespace
+        local table = { _name = basename }
+        if sdk.find_type_definition(basename) and not force_namespace then
+            table = { _typedef = sdk.find_type_definition(basename), _name = basename }
+        else
+            table = { _name = basename }
+        end
+        return setmetatable(table, namespace_builder_metatable)
+    end
+
+    return setmetatable({}, { __index = function(self, name)
+        return create_namespace(name)
+    end })
+end)()
+
+local snow = metatable.snow
+
+local function empty_pre_func(retval)
+    return retval
+end
+
+sdk.hook(
+    snow.gui.GuiSaveDataSelectMenu.isCharaEditable,
+    empty_pre_func,
+    function(retval) return sdk.to_ptr(true) end
+)
+
+sdk.hook(
+    snow.gui.fsm.charamake.GuiCharamakePlayerFsmManager.checkUseEditPlayerTicket,
+    empty_pre_func,
+    function(retval) return sdk.to_ptr(false) end
+)
